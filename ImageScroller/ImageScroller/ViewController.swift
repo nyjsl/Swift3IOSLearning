5f4a99cc90d84b04255a0b6117237c56cbb1a63d
= <=>
2016-11-08 22:01:42 +0800

    steveJobs

---
+++ ImageScroller/ImageScroller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ImageScroller
+//
+//  Created by 魏星 on 2016/11/8.
+//  Copyright © 2016年 wx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var steveJobs: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
